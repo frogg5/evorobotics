5b54983c634696593c8b9aa96df37a413e735100
Jeffrey Lam <75817943+frogg5@users.noreply.github.com>
2025-06-08 13:13:39 -0400

    Update new_knapsack.py

--- new_knapsack.py
+++ new_knapsack.py
@@ -1,7 +1,7 @@
 import random
 import copy
 import statistics
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 global knapsack_weight
 global knapsack_values
@@ -53,17 +53,17 @@ def parentselection_tournament(population_values, parameter_parentsamplesize): #
 		while r in candidates:
 			r = random.randint(0, len(population_values) - 1)
 		candidates.append(r) #select 5 candidates
-	print("candidates", candidates)
+	#print("candidates", candidates)
 	candidates_values = []
 	for candidate in candidates:
 		candidates_values.append(population_values[candidate])
-	print("candidates values", candidates_values)
+	#print("candidates values", candidates_values)
 	best_candidate = candidates[candidates_values.index(max(candidates_values))]
-	print("best_candidate_index", best_candidate)
+	#print("best_candidate_index", best_candidate)
 	return best_candidate #returns the index of the best candidate in population_values / population
 
 def mutation_allindex(individual): #selects all indexes, might change each one
-	parameter_allindex_random_mutation_rate = 15 #5% mutation rate / gene #the higher this goes up the higher the success rate
+	parameter_allindex_random_mutation_rate = 22 #5% mutation rate / gene #the higher this goes up the higher the success rate
 	i = 0
 	for item in individual:
 		#print(i)
@@ -80,32 +80,54 @@ def mutation_allindex(individual): #selects all indexes, might change each one
 
 def crossover_twopoint(parent1, parent2): #make two individuals not one
 	x = random.randint(1,len(parent1)-3)
-	y = random.randint(1,len(parent1)-1)
+	y = random.randint(1,len(parent1)-1) #maybe start with x, not 1
 	while(y<=x):
 		y = random.randint(1,len(parent1)-1)
-	print(x,y)
+	#print(x,y)
 	newindividual1 = parent1[:x] + parent2[x:y] + parent1[y:]
 	newindividual2 = parent2[:x] + parent1[x:y] + parent2[y:]
 	return newindividual1, newindividual2
 
+#uniform crossover
+#every gene has an equal chance of switching.
+#random org - makes a template. 0 1 0 1. But it means a gene from parent 1, parent 2.
+#screenshotted, 6/2 for reference
+
+#multipoint - uses a template to determine where crossover happens
+
+#when implementing generational, implement elitism
+#elitism 10%. Top 10% from old pop. stay
+
+def graph(listX, listY, nameX, nameY, title):
+	plt.plot(listX, listY)
+	plt.xlabel(nameX)
+	plt.ylabel(nameY)
+	plt.title(title)
+	plt.show()
+	print("graphed")
+
 def main():
-	cases_count = 1
-	evolutionary_cycle_count = 200
+	cases_count = 50
+	evolutionary_cycle_count = 550
+
+	highest_end_value = []
+	percent_count = 0
 
 	for i in range(cases_count):
-		population_individuals = generatePopulation(50)
+		paramter_populationsize = 100
+		population_individuals = generatePopulation(paramter_populationsize)
 		population_values = []
 		for individual in population_individuals:
 			population_values.append(evaluateIndividual(individual))
 		population = list(zip(population_individuals, population_values))
 		print("starting population values: ", population_values)
-		print("starting population: ", population)
+		#print("starting population: ", population)
 
 		for j in range(evolutionary_cycle_count):
 
 			print("CYCLE #", j)
 			#Find two parents
-			parameter_tournamentsize = 10
+			parameter_tournamentsize = 20
 			parent1 = parentselection_tournament(population_values, parameter_tournamentsize)
 			parent2 = parentselection_tournament(population_values, parameter_tournamentsize)
 			while parent1==parent2:
@@ -114,8 +136,8 @@ def main():
 			print("parents:", population[parent1], population[parent2])
 
 			#Chance of crossover happening. If crossover happens, then 2 offspring are created and maybe mutated.
-			parameter_crossoverchance = 80 #set to 80% NOT SURE HOW TO IMPLEMENT LOGICALLY
-			x = random.randint(1,100)
+			parameter_crossoverchance = 80
+			x = random.randint(1,100) #put this inside the crossover function
 			if x<=parameter_crossoverchance:
 				offspring = crossover_twopoint(population[parent1][0], population[parent2][0])
 				offspring1 = copy.deepcopy(offspring[0])
@@ -138,17 +160,34 @@ def main():
 				population_values[lowest_individual_index] = offspring2[1]
 				population[lowest_individual_index] = offspring2
 
+			#else: the two parents are the offspring without crossover. mutate(?) them. then replaced. restructure ~140-on
+
 			print("end cycle population values:", population_values)
-			print("end cycle population:", population)
+			#print("end cycle population:", population)
 
 		#cycle is over, determine:
 		highest_organism_value = max(population_values)
 		highest_organism_index = population_values.index(highest_organism_value)
 		highest_organism = population[highest_organism_index]
 		print("final result population values", population_values)
+		print("final result population", population)
 		print(highest_organism)
+		highest_end_value.append(highest_organism[1])
+		if highest_organism[1] == answer:
+			percent_count = percent_count + 1
+
+	print("success rate: ", percent_count/cases_count)
+
+	#graphing
+	x_axis_trials = []
+	for i in range(cases_count):
+		x_axis_trials.append(i)
+	graph(x_axis_trials, highest_end_value, "Trail #", "End result", "End result vs Trial number")
 
 if __name__ == "__main__":
 	main()
 
 #5/27 - look in DEAP
+# is my implementation correct
+# at what point does it just become random-brute force-until-right?
+# how do i implement generational GA
